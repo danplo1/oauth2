b3645a90308c5d8bf02612655381e8dc47ff6396
danplo1 <budweiser401@wp.pl>
2019-03-22 21:02:50 +0100

    Initial commit

---
+++ src/main/java/pl/danplo/oauth2/LoginApi.java
@@ -0,0 +1,33 @@
+package pl.danplo.oauth2;
+
+
+import com.sun.javafx.scene.traversal.Algorithm;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+
+@RestController
+public class LoginApi {
+
+
+    @PostMapping("/logIn")
+    public String login(@RequestBody User user) {
+        long currentTimeMillis = System.currentTimeMillis();
+        //token
+        return Jwts.builder()
+                .setSubject(user.getLogin())
+                .claim("roles", "user")
+                .setIssuedAt(new Date(currentTimeMillis))
+                .setExpiration(new Date(currentTimeMillis + 20000))
+                .signWith(SignatureAlgorithm.HS512, user.getPassword())
+                .compact(); // metoda zwraca stringa
+
+
+    }
+
+}
